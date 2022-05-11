b9ae81579409f406040f58acc85729de52581c78
RomanNikitchenko <nikitchenkoroman25@gmail.com>
2022-05-11 17:32:49 +0300

    ht2-2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,56 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import React from 'react';
+
+class App extends React.Component {
+  state = {
+    contacts: [],
+    name: ''
+  }
+
+  handleChange = evt => {
+    const { name, value } = evt.target;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = evt => {
+    evt.preventDefault();
+    const { name, contacts } = this.state;
+    contacts.push(name);
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({
+      name: ''
+    });
+  };
+
+  render() {
+    const { name, contacts } = this.state;
+
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name
+            <input
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+              value={name}
+              onChange={this.handleChange}
+            />
+          </label>
+
+          <button type="submit">Add contact</button>
+        </form>
+        <ul>
+          {contacts.map(contact => ( <li key={contact}>{contact}</li> ))}
+        </ul>
+      </div>
+    );
+  }
 };
+
+export default App
